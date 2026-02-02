4e6c95c3180db2a6d8370f3bd7e13538c8226988
Drew CodeRGV <drew@codergv.org>
2026-02-02 00:00:37 -0600

    feat: Add SSID inheritance support and latest edge files

--- deploy_to_raspi_v5.sh
+++ deploy_to_raspi_v5.sh
@@ -704,11 +704,6 @@ wmm_enabled=0
 macaddr_acl=0
 auth_algs=1
 ignore_broadcast_ssid=0
-wpa=2
-wpa_passphrase=crowdsurfer2024
-wpa_key_mgmt=WPA-PSK
-wpa_pairwise=TKIP
-rsn_pairwise=CCMP
 EOF
 
 # Configure dnsmasq for management interface
@@ -773,11 +768,6 @@ wmm_enabled=0
 macaddr_acl=0
 auth_algs=1
 ignore_broadcast_ssid=0
-wpa=2
-wpa_passphrase=crowdsurfer2024
-wpa_key_mgmt=WPA-PSK
-wpa_pairwise=TKIP
-rsn_pairwise=CCMP
 EOF
         
         # Configure network interface for client WiFi
